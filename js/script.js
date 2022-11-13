61b04ae251ed65ef1c94cdb518ec80db3c1b3db9
nickyesteban <nicky.paul.esteban@gmail.com>
2022-11-13 13:58:55 +0800

    first commit

--- js/script.js
+++ js/script.js
@@ -7,11 +7,14 @@ const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+let historyList = document.querySelector("#historyList");
+
 
 function getComputerChoice() {
     const choices = ["r", "p", "s"];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
+
 }
 
 function convertToWord(letter) {
@@ -27,6 +30,9 @@ function win(userChoice, computerChoice) {
     result_div.innerHTML = convertToWord(userChoice) + " beats " + convertToWord(computerChoice) + ". You win! 🔥"
     document.getElementById(userChoice).classList.add("green-glow");
     setTimeout(function () { document.getElementById(userChoice).classList.remove("green-glow") }, 500);
+
+
+
 }
 
 
@@ -49,23 +55,38 @@ function draw(userChoice, computerChoice) {
     setTimeout(function () { document.getElementById(userChoice).classList.remove("gray-glow") }, 500);
 }
 
+
 function game(userChoice) {
     const computerChoice = getComputerChoice();
+    let historyItem = document.createElement("li");
+
     switch (userChoice + computerChoice) {
         case "pr":
         case "rs":
         case "sp":
+
             win(userChoice, computerChoice);
+            historyItem.textContent = "User defeated Computer 🔥";
+            historyItem.style.backgroundColor = "green";
+            historyList.appendChild(historyItem);
+
+
             break
         case "rp":
         case "ps":
         case "sr":
             lose(userChoice, computerChoice);
+            historyItem.textContent = "Computer defeated User 😭";
+            historyItem.style.backgroundColor = "red"
+            historyList.appendChild(historyItem);
             break
         case "rr":
         case "pp":
         case "ss":
             draw(userChoice, computerChoice);
+            historyItem.textContent = "Draw 😑";
+            historyItem.style.backgroundColor = "black"
+            historyList.appendChild(historyItem);
             break
     }
 }
